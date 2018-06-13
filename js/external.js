12ceb7d1cca1fbdda0fe2e8740094bfa8e076d67
La Mar Lofton <lamarlofton@Las-MacBook-Air.local>
2018-06-13 09:17:05 -0500

    I hope this is going to my repo

---
+++ js/external.js
@@ -0,0 +1,65 @@
+
+
+console.log('Hello from external JavaScript');
+
+
+
+    // alert("Welcome to my Website!");
+    //
+    //     var userColor = prompt('What is your favorite color?');
+    //     console.log('user color: ' + userColor);
+    //     confirm('That\'s my favorite color too!');
+    //
+    //     var userMovies = prompt('How many days did you rent these movie for?');
+    //     console.log('user movies: ' + userMovies);
+    //     confirm('Rentals per day are $3.00');
+    //     alert('Rental Summary');
+    //
+    //     var userConfirm = confirm('You rented Little Mermaid for 3days, Brother Bear 5days, and Hercules 1day');
+    //     alert('that will be $27.00');
+    //     console.log('user confirm: ' + userConfirm);
+
+
+
+
+
+        // var userGoogle = confirm('Google offers $400 per hour');
+        // console.log('user Google: ' + userGoogle);
+        // confirm('So at 4hours');
+        // alert('You earned $1,600.00');
+        //
+        // var userAmazon = confirm('Amazon offers $380 per hour');
+        // console.log('user Amazon: ' + userAmazon);
+        // confirm('At 6hours');
+        // alert('You earned $2,280.00');
+        //
+        // var userFacebook = confirm('Facebook offers $350 per hour');
+        // console.log('user Amazon: ' + userAmazon);
+        // confirm('At 10hours');
+        // alert('You earned $3,500.00');
+        //
+        // alert('Your total wages = 7,380.00');
+
+
+
+// var YouCanEnroll = confirm('Are you sure that the class is not full and does not conflict with your schedule?');
+//
+// if (YouCanEnroll) {
+//     // class not full and does not conflict...
+// } else {
+//     alert('Class Is Full!!');
+// }
+
+
+
+    // var IamAPremium = confirm('Are you a Premium member?');
+    // console.log('IamAPremium: ' + IamAPremium);
+    //
+    // if (IamAPremium) {
+    //
+    // } else if ('If not a Premium member must buy two or more product') {
+    //     alert('You Must buy 2 or more products!!');
+    //     console.log('IamAPremium: ' + IamAPremium);
+    //
+    //     alert('Purchase two product now!!');
+    //     }
