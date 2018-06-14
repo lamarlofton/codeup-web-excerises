fdf1bc13416a78b7d7ef02a79dadef5a41c351ed
La Mar Lofton <lamarlofton@Las-MacBook-Air.local>
2018-06-14 09:58:42 -0500

    going to the branch

---
+++ js/function-warmup.js
@@ -0,0 +1,15 @@
+    function makeUsername(firstName, age) {
+        return (firstName.length) + (firstName.charAt(0) .toLowerCase() + age);
+    }
+
+
+   var username = makeUsername('John', 20);
+ console.log('John', 20, username);
+
+
+    function makeUsername(firstName, age) {
+        return (firstName.length) + (firstName.charAt(0) .toLowerCase() + age);
+    }
+
+      var username = makeUsername('Sandy', 25);
+    console.log('Sandy', 25, username);
