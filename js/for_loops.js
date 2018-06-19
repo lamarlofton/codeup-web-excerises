1ad8646f79921267546f932e9570462092c4f155
La Mar Lofton <lamarlofton@Las-MacBook-Air.local>
2018-06-19 10:22:37 -0500

    getting my work in

---
+++ js/for_loops.js
@@ -0,0 +1,58 @@
+
+
+
+// // function showMultiplicationTable(num) {
+//     for (var i = 7; i < 70; i++) {
+//         console.log('for loop iteration #' + i + '');
+//     }
+// // }
+
+
+
+
+// for (var i = 0; i < 10; i++) {
+//     console.log('for loop iteration #' + i);
+// }
+
+
+
+// var num = 7;
+// for(var i = 1; i<=10; i+=1) {
+//     console.log(num,' * ', i, ' = ', num*i);
+// }
+
+
+
+// var num = parseInt(prompt("enter any number"));
+// for(var i = 1; i<=10; i++) {
+//     console.log("<br>", num," * ", i, " = ", num*i);
+// }
+
+
+// function getRandomNumber() {
+//     for (var i = 20; i < Math.floor(Math.random() * 100);  i++);
+//     console.log(i);
+//         if ((i % 2) === 0) {
+//             console.log('Even Number');
+//         }
+//         else {
+//             console.log('Odd Number');
+//         }
+//     }
+
+
+// getRandomNumber();
+
+//
+// for(var i = 100; i > 0; i = i - 5){
+//     console.log(i);
+// }
+
+
+for (var i = 1; i<=9; i +=1) {
+    var output = "";
+    for (var j = 1; j <= i; j += 1){
+        output += i;
+    }
+    console.log(output);
+}
---
+++ js/while.js
@@ -0,0 +1,50 @@
+
+
+
+// var numberByTwos = 2;
+//
+// for (var i = 2; i < 65536; i++) {
+//     console.log('Loop counter is:' * i);
+//     if (i === numbersByTwos) {
+//         break;
+//         console.log('done!!!');
+//     }
+// }
+//
+//
+// var i = 65536;
+//
+// do {
+//     console.log('while loop iteration #' + i);
+//     i++;
+// } while (i < 65536);
+
+//
+// var x = 2;
+// while(x < 65538){
+//     console.log(x++);
+//     x = x * 2;
+// }
+
+
+// // This is how you get a random number between 50 and 100
+// var allCones = Math.floor(Math.random() * 50) + 50;
+// // This expression will generate a random number between 1 and 5
+//
+// do {
+//     console.log('Start of the day i have ' + allCones + ' left');
+//
+//    var conesToSell = Math.floor(Math.random() * 5) + 1;
+//
+//         if (conesToSell <= allCones) {
+//             console.log('I sold ' + conesToSell);
+//             allCones = allCones - conesToSell;
+//         }else if (allCones === 0) {
+//             console.log('I can go home');
+//         }else{
+//             console.log('I don\'t have  ' +conesToSell+' cones to sell');
+//         }
+//
+//
+// } while(allCones !== 0);
+
